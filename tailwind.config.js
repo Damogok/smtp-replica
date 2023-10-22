71bb827ecc7ee17447fdd7c05f1b36d121e3e794
my pc <mypc@mys-MacBook-Pro.local>
2023-10-22 13:21:57 +0100

    first section done

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors :{
+        'primary-blue' : '#0C2360',
+        'button-color' : '#1CD2A1'
+      }
+    },
+  },
+  plugins: [],
+}
